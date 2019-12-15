9c7508ec31749f2cb6a051400b9471ec0204b6ec
Edvin Dzidic <edvindzidic2000@gmail.com>
2019-12-16 00:47:52 +0100

    Add error page

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import { BrowserRouter, Route, Switch, withRouter, Router} from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Home from '../components/Home';
 import Privacy from '../components/Privacy';
 import Terms from '../components/Terms';
+import NoRoute from '../components/NoRoute';
 
 const AppRouter = () => (
     <BrowserRouter>
@@ -14,10 +15,11 @@ const AppRouter = () => (
                 <Route path='/' component={Home} exact={true}/>
                 <Route path='/privacy' component={Privacy}/>
                 <Route path='/terms' component={Terms}/>
+                <Route path='/' component={NoRoute}/>
             </Switch>
             <Footer />
         </React.Fragment>
     </BrowserRouter>
 );
 
-export default AppRouter;
+export default AppRouter;   
