cc82ce8dae37a96f3136fe3eaf0dfe0f9ae1e020
Edvin Dzidic <edvindzidic2000@gmail.com>
2019-12-28 19:18:43 +0100

    Added little icon, half of functionality of input on newlist

--- src/components/NewList.js
+++ src/components/NewList.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import edit from '../pictures/edit.png';
 
-const NewList = () => (
-    <div className='newlist'>
-        <p className='newlist__placeholdername'>(NAME THIS LIST)</p>
+const NewList = () => {
+
+    const [listName, setListName ] = useState('');
+    
+    return(
+        <div className='newlist'>
+        <button className='newlist__placeholder'>
+            <div className='newlist__placeholder__area'>
+                <p className='newlist__placeholder__area__name'>{listName === '' ? '(NAME THIS LIST)' : listName}</p>
+                <img className='newlist__placeholder__area__icon' src={edit} alt='edit icon'></img>
+                </div>
+        </button>
         <input type='text' className='newlist__taskinput' placeholder='Add a new task...'></input>
         <div className='newlist__filterarea'>
             <p>Show:</p>
@@ -11,6 +21,7 @@ const NewList = () => (
             <button className='newlist__filterareabutton'>PENDING</button>
         </div>
     </div>
-);
+    );
+};
 
 export default NewList;
