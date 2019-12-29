36a20eee99ef40184a73a65d3b432a9578c32b10
Edvin Dzidic <edvindzidic2000@gmail.com>
2019-12-29 13:02:08 +0100

    Finish 90% input functionality

--- src/components/NewList.js
+++ src/components/NewList.js
@@ -1,18 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import edit from '../pictures/edit.png';
 
 const NewList = () => {
 
     const [listName, setListName ] = useState('');
+    const [listNameState, setListNameState] = useState('button');
+    const textInput =  useRef();
+    const changeToInput = () => {
+        setListNameState('input');
+        setTimeout(() => {
+            textInput.current.focus();
+        },0);
+    }
     
     return(
         <div className='newlist'>
-        <button className='newlist__placeholder'>
-            <div className='newlist__placeholder__area'>
-                <p className='newlist__placeholder__area__name'>{listName === '' ? '(NAME THIS LIST)' : listName}</p>
-                <img className='newlist__placeholder__area__icon' src={edit} alt='edit icon'></img>
+        {listNameState === 'button' && (
+            <button 
+            onClick={changeToInput}
+            className='newlist__placeholder'>
+                <div className='newlist__placeholder__area'>
+                    <p className='newlist__placeholder__area__name'>{listName === '' ? '(NAME THIS LIST)' : listName}</p>
+                    <img className='newlist__placeholder__area__icon' src={edit} alt='edit icon'></img>
                 </div>
-        </button>
+            </button>
+        )}
+        {listNameState === 'input' && 
+        <input 
+            onBlur={(e) => setListNameState('button')} 
+            onFocus={(e) => e.target.placeholder = listName} 
+            placeholder='(NAME THIS LIST)' 
+            className='newlist__input'
+            value={listName}
+            onChange={(e) => setListName((e.target.value).toUpperCase())}
+            ref={textInput}>
+        </input>}
         <input type='text' className='newlist__taskinput' placeholder='Add a new task...'></input>
         <div className='newlist__filterarea'>
             <p>Show:</p>
