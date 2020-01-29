410784a4f24b695f65dfa6edb637a5eb1a48e500
Edvin Dzidic <edvindzidic2000@gmail.com>
2020-01-29 22:02:32 +0100

    Start signup functionality

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { firebase } from '../firebase/firebase';
 
-const Signup = () => (
-    <div className='main-flex-container'>
+const Signup = () => {
+
+    const [username, updateUsername] = useState('');
+    const [email, updateEmail] = useState('');
+    const [password, updatePassword] = useState('');
+    const [passwordRepeat, updatePasswordRepeat] = useState('');
+
+    const handleSignUp = () => {
+        const emailTest = /\S+@\S+/;
+        if(username.length > 4 && emailTest.test(email) && password.length > 4 && password === passwordRepeat){
+            alert('Successfully signed up!')
+        }else{
+            console.log('Hell naws, bruh!');
+        }
+    }
+
+    return (
+        <div className='main-flex-container'>
         <Link className='form__home-button' to='/'> COLABI.CO </Link>
         <div className='signup__form'>
-            <input className='signup__input' placeholder='Username'></input>
-            <input className='signup__input' placeholder='Email'></input>
-            <input className='signup__input' placeholder='Password'></input>
-            <input className='signup__input' placeholder='Repeat Password'></input>
+            <input onChange={(e) => updateUsername(e.target.value)} className='signup__input' placeholder='Username'></input>
+            <input onChange={(e) => updateEmail(e.target.value)} className='signup__input' placeholder='Email'></input>
+            <input type='password' onChange={(e) => updatePassword(e.target.value)} className='signup__input' placeholder='Password'></input>
+            <input type='password' onChange={(e) => updatePasswordRepeat(e.target.value)} className='signup__input' placeholder='Repeat Password'></input>
             <div className='signup__form__buttons'>
-                <button className='signup__form__button'>SIGN UP</button>
+                <button onClick={handleSignUp} className='signup__form__button'>SIGN UP</button>
             </div>
         </div>
         <Link to='/login' className='signup-form-login'>
@@ -18,6 +35,7 @@ const Signup = () => (
         </Link>
         <p className='signup__form__terms'>By signing up, you agree to our <a href='/terms'>Terms of Use</a> and <a href='/privacy'>Privacy Policy</a>.</p>
     </div>
-);
+    );
+}
 
 export default Signup;
