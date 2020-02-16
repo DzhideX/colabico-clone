11a1ade0a60076df3ef3d94861067a74afb5e022
Edvin Dzidic <edvindzidic2000@gmail.com>
2020-02-16 13:24:51 +0100

    Start home list boxes

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,17 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import  database  from '../firebase/firebase';
 
+const ListBox = ({listName, lastTodo, numberOfTodos,className}) => {
+
+
+    return(
+        <div className={className}>
+           
+        </div>
+    );
+}
+
+
 
 const Home = ({userId}) => {
 
@@ -10,17 +21,31 @@ const Home = ({userId}) => {
         if(userId){
             console.log(userId);
             database.ref(`users/${userId}`).once('value').then(snapshot => {
-                console.log(snapshot.val());
+                Object.keys(snapshot.val()).map((key,i) => {
+                    console.log(`This key: ${key} is number ${i}`);
+                })
+                
+                updateListNames(snapshot.val());
             })
         }
     },[userId]);
+
+    const setListBoxClass = (key) => {
+        if(key === 0){
+            return "home__listbox--top";
+        }else if(key+1 === Object.keys(listNames).length){
+            return "home__listbox--bottom"
+        }else{
+            return "home__listbox"
+        }
+    }
 
     const [listNames, updateListNames ] = useState();
 
     return (
         <div className='main-flex-container'>
             <Link to='/l/new' className='home__button'> NEW LIST </Link>
-            <p className='home__infotext'> Start by pressing that big button up there! </p>
+            {listNames ? Object.keys(listNames).map((key,i) => <ListBox className={setListBoxClass(i)}/>) : <p className='home__infotext'> Start by pressing that big button up there! </p>}
         </div>
     );
 }
