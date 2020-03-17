6ea09b448bf49f76d064ef17e0c12cd23de393dd
Edvin Dzidic <edvindzidic2000@gmail.com>
2020-03-17 22:25:36 +0100

    Move sensitive data to env, try out one of the routes and its implementation

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,59 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const firestore = require("./config/firebase");
+const { db } = require("./config/firebase");
 
 dotenv.config();
 const app = express();
 
-app.get("/", (req, res) => {
-  firestore.db
-    .collection("users")
+app.get("/user/:userid/listdata", (req, res) => {
+  let Arr = [];
+  db.collection(`users/${req.params.userid}/lists`)
     .get()
-    .then(docRef => {
-      let html = `There are ${docRef.docs.length} user in our database, and their ids are: `;
-      docRef.docs.map(user => {
-        html += `${user.id}, `;
+    .then(snapshot => {
+      snapshot.docs.forEach((doc, i) => {
+        db.collection(`users/${req.params.userid}/lists/${doc.id}/todos`)
+          .get()
+          .then(snapshotTwo => {
+            let tempObj = {
+              name: doc.data() ? doc.data().name : "",
+              lastTodo: snapshotTwo.docs[snapshotTwo.docs.length - 1]
+                ? snapshotTwo.docs[snapshotTwo.docs.length - 1].data().value
+                : "",
+              numberOfTodos: snapshotTwo.docs.length,
+              id: doc.id
+            };
+            Arr.push(tempObj);
+          })
+          .then(() => {
+            if (Arr.length === snapshot.docs.length) {
+              // resolve(Arr);
+              let tempString = "List names from that user: ";
+              Arr.map((list, i) => {
+                tempString += `\nName: ${list.name}, last todo: ${list.lastTodo}, id: ${list.id}`;
+              });
+              res.send(Arr);
+              // res.json(Arr);
+            }
+          });
       });
-      res.send(html);
     });
+});
+
+app.get("*", (req, res) => {
+  res.send("404");
 });
 
 app.listen(process.env.PORT, () => {
   console.log(`server is up on port ${process.env.PORT}!`);
 });
+
+// THE ROUTES THAT WE NEED ARE:
+// ADDLISTSAGA - POST REQUEST -- ADDS A NEW LIST
+// ADDTODOSAGA - POST REQUEST -- ADDS A NEW TODO
+// CHANGELISTNAMESAGA - PUT REQUEST -- UPDATE A LIST NAME
+// DELETELISTSAGA - DELETE REQUEST -- DELETE A LIST
+// DELETETODOSAGA - DELETE REQUEST -- DELETE A TODO
+// LISTDATASAGA - GET REQUEST -- GET LIST DATA
+// SETTODOSTATESAGA - PUT REQUEST -- UPDATE TODO STATE
+// SETTODOVALUESAGA - PUT REQUEST -- UPDATE TODO VALUE
+// TODOSSAGA && USERIDSAGA - GET REQUEST X2 -- GET TODOS AND USERID
--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,14 +1,17 @@
 const firebase = require("firebase");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 var firebaseConfig = {
-  apiKey: "AIzaSyCLCbp5cV2oubWbbZ3vqa4nR4rQKbS5qqU",
-  authDomain: "colabico-882e0.firebaseapp.com",
-  databaseURL: "https://colabico-882e0.firebaseio.com",
-  projectId: "colabico-882e0",
-  storageBucket: "colabico-882e0.appspot.com",
-  messagingSenderId: "970527431676",
-  appId: "1:970527431676:web:daae16e70b18eb21297079",
-  measurementId: "G-FJZ5VEK809"
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  databaseURL: process.env.DATABASEURL,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINSENDERID,
+  appId: process.env.APPID,
+  measurementId: process.env.MEASUREMENTID
 };
 
 firebase.initializeApp(firebaseConfig);
