531c4762ac644a8d3f5807e8b84c8e9b0c64fe04
Edvin Dzidic <edvindzidic2000@gmail.com>
2020-06-05 23:15:21 +0200

    Fix app crashing when reloading or manually typing in route

--- frontend/server/server.js
+++ frontend/server/server.js
@@ -1,15 +1,16 @@
 import express from 'express';
 import fs from 'fs';
 import path from 'path';
-
-const app = express();
-
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 
 import App from '../src/index';
 
-app.use('^/$', (req, res, next) => {
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
+
+app.use('*', (req, res, next) => {
   fs.readFile(path.resolve('./build/index.html'), 'utf-8', (err, data) => {
     if (err) {
       console.log(err);
@@ -24,7 +25,6 @@ app.use('^/$', (req, res, next) => {
   });
 });
 
-app.use(express.static(path.resolve(__dirname, '..', 'build')));
 app.listen(3000, () => {
   console.log('Listening...');
 });
